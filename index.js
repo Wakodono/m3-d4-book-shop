aea8f9d092496f37016c70e100201408b5291616
Wakodono <53127265+Wakodono@users.noreply.github.com>
2021-09-16 15:56:48 +0100

    Add to cart button
    On click add different styling

--- index.js
+++ index.js
@@ -19,13 +19,13 @@ window.onload = () => {
         row.innerHTML = allProducts
           .map(
             (books) => `
-              <div class="col-12 col-sm-6 col-md-3">
+              <div id="book-wrap" class="col-12 col-sm-6 col-md-3">
                   <div class="card">
                       <img src="${books.img}" class="card-img-top img-fluid w=100 object-fit: cover" alt="...">
                       <div class="card-body">
                       <h5 class="card-title">${books.title}</h5>
                       <p class="card-text">${books.price}</p>
-                      <a href="#" class="btn btn-primary">Add to Cart</a>
+                      <a href="#" class="btn btn-primary" onClick="selected()">Add to Cart</a>
                       <a href="#" class="btn btn-secondary">Skip</a>
                       </div>
                   </div>
@@ -33,4 +33,8 @@ window.onload = () => {
           `
           )
           .join("");
+      }
+
+      const selected = () => {
+          document.getElementById("book-wrap").style.border = "1px solid red"
       }
